2ace5137b45b0eaa0c077f2985536663ae40a9b5
Michael <L00170144@atu.ie>
2024-01-14 21:08:03 +0000

    updated controllers for post endpoint

--- Global-Green-Initiative/src/main/java/com/group2/GlobalGreenInitiative/controller/CustomerController.java
+++ Global-Green-Initiative/src/main/java/com/group2/GlobalGreenInitiative/controller/CustomerController.java
@@ -1,14 +1,20 @@
 package com.group2.GlobalGreenInitiative.controller;
 
+import com.group2.GlobalGreenInitiative.dao.CustomerRepository;
 import com.group2.GlobalGreenInitiative.entities.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customers")
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
 
     @GetMapping("/new")
     public String displayCustomerForm(Model model) {
@@ -17,5 +23,12 @@ public class CustomerController {
 
         model.addAttribute("customer", aCustomer);
         return "customer";
+    }
+
+    @PostMapping("/save")
+    public String createCustomer(Customer customer, Model model) {
+        customerRepository.save(customer);
+
+        return "redirect:/Home/new";
     }
 }
