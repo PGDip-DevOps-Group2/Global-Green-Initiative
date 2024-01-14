4a5359b4febdba32cb034d94e52732da55c1c35f
Michael <L00170144@atu.ie>
2024-01-14 20:46:21 +0000

    added entitie for grant

--- Global-Green-Initiative/src/main/java/com/group2/GlobalGreenInitiative/controller/GrantController.java
+++ Global-Green-Initiative/src/main/java/com/group2/GlobalGreenInitiative/controller/GrantController.java
@@ -1,12 +1,8 @@
-package com.group2.group2.controller;
-import com.group2.group2.dao.GrantRepository;
-import com.group2.group2.entities.Customer;
-import com.group2.group2.entities.Grant;
-import org.springframework.beans.factory.annotation.Autowired;
+package com.group2.GlobalGreenInitiative.controller;
+import com.group2.GlobalGreenInitiative.entities.Grant;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/grants")
