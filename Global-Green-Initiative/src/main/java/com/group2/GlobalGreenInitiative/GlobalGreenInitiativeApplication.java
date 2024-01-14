edef60d93a1a217ca7274b766ecfdbcee6f5573e
Michael <L00170144@atu.ie>
2024-01-14 12:05:23 +0000

    adding intitial commit, Springboot start up

---
+++ Global-Green-Initiative/src/main/java/com/group2/GlobalGreenInitiative/GlobalGreenInitiativeApplication.java
@@ -0,0 +1,13 @@
+package com.group2.GlobalGreenInitiative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalGreenInitiativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalGreenInitiativeApplication.class, args);
+	}
+
+}
